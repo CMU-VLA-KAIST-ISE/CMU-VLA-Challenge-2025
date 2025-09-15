05458e15b8d50aa6636e3be4a9b07232764edada
Wonhyeok <choewonhyeog3@gmail.com>
2025-09-16 00:00:50 +0900

    Fixed

--- launch_module.sh
+++ launch_module.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Python 스크립트 실행 권한 설정
+echo "Setting executable permissions for Python scripts..."
+find /home/cwh316/CMU-VLA-Challenge -name "*.py" -type f -exec chmod +x {} \;
+find /home/cwh316/CMU-VLA-Challenge -name "*.cpp" -type f -exec chmod +x {} \;
+find /home/cwh316/CMU-VLA-Challenge -name "*.launch" -type f -exec chmod +x {} \;
+
 sleep 3
-source ai_module/devel/setup.bash
-roslaunch dummy_vlm dummy_vlm.launch
+# interaction_manager와 color_scan_generation을 함께 실행
+roslaunch interaction_manager interaction_manager.launch 
+# 모든 백그라운드 프로세스가 완료될 때까지 대기
+wait
